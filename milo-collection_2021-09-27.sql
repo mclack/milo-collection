1620a3ad570d2af084b938176281d9de4990fd0d
mclack <miloclack@hotmail.co.uk>
2021-09-27 13:36:18 +0100

    image field added and filled in database. Images found and saved in images directory

--- milo-collection_2021-09-27.sql
+++ milo-collection_2021-09-27.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.35)
 # Database: milo-collection
-# Generation Time: 2021-09-27 11:02:05 +0000
+# Generation Time: 2021-09-27 12:31:50 +0000
 # ************************************************************
 
 
@@ -32,20 +32,21 @@ CREATE TABLE `idols` (
   `instrument` varchar(20) NOT NULL DEFAULT '',
   `band` varchar(20) NOT NULL DEFAULT '',
   `technical prowess` int(2) unsigned NOT NULL,
+  `image` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `idols` WRITE;
 /*!40000 ALTER TABLE `idols` DISABLE KEYS */;
 
-INSERT INTO `idols` (`id`, `name`, `age`, `instrument`, `band`, `technical prowess`)
+INSERT INTO `idols` (`id`, `name`, `age`, `instrument`, `band`, `technical prowess`, `image`)
 VALUES
-	(1,'Mario Duplantier',40,'Drums','Gojira',10),
-	(2,'Troy Sanders',48,'Bass + Vocals','Mastodon',6),
-	(3,'Javier Reyes',40,'8-String Guitar','Animals As Leaders',10),
-	(4,'Mike Scheidt',51,'Guitar + Vocals','YOB',7),
-	(5,'Matt Ryan',32,'Vocals','Desert Storm',7),
-	(6,'Devin Townsend',49,'Guitar + Vocals','Devin Townsend',9);
+	(1,'Mario Duplantier',40,'Drums','Gojira',10,'marioduplantier.jpg'),
+	(2,'Troy Sanders',48,'Bass + Vocals','Mastodon',7,'troysanders.jpg'),
+	(3,'Javier Reyes',40,'8-String Guitar','Animals As Leaders',10,'javierreyes.jpg'),
+	(4,'Mike Scheidt',51,'Guitar + Vocals','YOB',8,'mikescheidt.jpg'),
+	(5,'Matt Ryan',32,'Vocals','Desert Storm',7,'mattryan.jpg'),
+	(6,'Devin Townsend',49,'Guitar + Vocals','Devin Townsend',9,'devintownsend.jpg');
 
 /*!40000 ALTER TABLE `idols` ENABLE KEYS */;
 UNLOCK TABLES;

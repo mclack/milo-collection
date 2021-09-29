20498281f691221eb5ada48c3484ea24f3277add
mclack <miloclack@hotmail.co.uk>
2021-09-29 12:25:38 +0100

    Formatting for docblocks has been corrected

--- functions.php
+++ functions.php
@@ -1,25 +1,37 @@
 <?php
 
-// Creates connection to collection database
-// and sets fetch mode so that it will return an associative array
+/**
+ * Creates connection to collection database
+ * @return PDO The Db connection
+ */
 function getDb() {
     $db = new PDO ('mysql:host=db; dbname=milo-collection', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    // Returns the contents of the database as an object
     return $db;
 }
 
-// Takes the database object and runs an SQL query against it
-function getIdols($db) {
+/**
+ * Takes the PDO and runs an SQL query against it
+ *
+ * @param PDO $db the database connection
+ *
+ * @return array The results from our db query
+ */
+function getIdols(PDO $db) : array {
     $query = $db->prepare("SELECT `name`, `age`, `instrument`, `band`, `technical-prowess`, `image` FROM `idols`;");
     $query->execute();
     $idols = $query->fetchAll();
-//  Returns an associative array which contains nested arrays of each idol and their stats
     return $idols;
 }
 
-// Takes the array of $idols (created by getIdols()) as an argument and loops over the items,
-// adding appropriate HTML tags to allow for convenient display on index.php
+
+/**
+ * Takes $idols array (created by getIdols()) and loops over the items,
+    adding appropriate HTML tags to allow for convenient display on index.php
+ * @param array $idols the associative array of idols
+ * @return string returns each idol in a separate <div> with each stat being separated by <p>s
+    apart from the idol image which is stored in an <img> tag
+ */
 function displayItems(array $idols) : string {
     if(!count($idols)) {
         return 'Input error. Array doesn\'t exist';
@@ -33,8 +45,6 @@ function displayItems(array $idols) : string {
                 '<p>' . 'Most Notable Band: ' . $idol['band'] . '</p>' .
                 '<p>' . 'Technical Prowess: ' . $idol['technical-prowess'] . '</p>' . '</div>';
         }
-        // returns each idol in a separate <div> with each stat being separated by <p>s
-        // apart from the idol image which is stored in an <img> tag
         return $result;
 }
 

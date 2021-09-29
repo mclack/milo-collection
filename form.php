5dbad4ddf946d03d3b5a29a2fc3d063d1fa391cc
mclack <miloclack@hotmail.co.uk>
2021-09-29 10:17:33 +0100

    <label>s added to each input

--- form.php
+++ form.php
@@ -15,15 +15,28 @@ if(isset($_POST['name'])) {
 </head>
 
 <body class="form">
-    <form method="post" action="index.php">
-        <input type="text" name="name" placeholder="Name of Idol" required />
-        <input type="number" name="age" placeholder="Age of Idol" min="10" max="99" required />
-        <input type="text" name="band" placeholder="Idol's Most Notable Band" required />
-        <input type="text" name="instrument" placeholder="Idol's Instrument(s)" required />
-        <input type="number" name="technical-prowess"
+    <form method="post" action="index.php" id="injection">
+        <label form="injection" for="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Name of Idol" required />
+        <br>
+        <label form="injection" for="age">Age</label>
+        <input type="number" id="age" name="age" placeholder="Age of Idol" min="10" max="99" required />
+        <br>
+        <label form="injection" for="band">Band</label>
+        <input type="text" id="band" name="band" placeholder="Idol's Most Notable Band" required />
+        <br>
+        <label form="injection" for="instrument">Instrument</label>
+        <input type="text" id="instrument" name="instrument" placeholder="Idol's Instrument(s)" required />
+        <br>
+        <label form="injection" for="technical-prowess">Technical Prowess</label>
+        <input type="number" id="technical-prowess" name="technical-prowess"
                placeholder="Technical Prowess out of 10" min="1" max="10" required />
-        <input type="text" name="image" placeholder="Image File Name" />
+        <br>
+        <label form="injection" for="image">Image</label>
+        <input type="text" id="image" name="image" placeholder="Image File Name" />
+        <br>
         <input class="submit" type="submit" value="Submit" />
+        <br>
     </form>
 </body>
 
